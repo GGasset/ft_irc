290bf5f41dc32395e302d0e423fb6e7e60a09732
GGasset <gassetgerman@gmail.com>
2025-11-03 12:44:32 +0100

    added declarations

--- Socket/Server.hpp
+++ Socket/Server.hpp
@@ -1,16 +1,36 @@
 #include <cstddef>
+#include <string>
 #include <vector>
 #include <tuple>
 
 #include "User.hpp"
+#include "Channel.hpp"
 
 class Server
 {
 private:
-	size_t 				max_id;
+	std::vector<User>	loaded_users;
+
+	size_t 				max_client_id = 0;
+	size_t				max_server_id = 0;
 
 	std::vector<int>	client_fds;
 	std::vector<User>	clients;
+	std::vector<Channel> servers;
+
+	void write_user(User user, std::ofstream stream);
+	User read_user();
+
+	void route_message(std::string msg, User sender);
 
 public:
+
+	// 	Returns true on errors
+	int write_data_to_file(std::string path);
+
+	// Returns true on errors
+	int load_from_file(std::string path);
+
+	void handle_message(size_t client_i, void *msg, size_t msg_len);
+	void loop();
 };
