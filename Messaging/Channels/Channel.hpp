a251913af9ab29353749fb1f1dedf53d1caf4d91
GGasset <gassetgerman@gmail.com>
2025-11-07 13:04:26 +0100

    modified fields

--- Messaging/Channels/Channel.hpp
+++ Messaging/Channels/Channel.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <vector>
+#include "string"
 
 struct channel_mode
 {
 	// Empty passw for no passw
-	std::string passw = "";
+	std::string passw;
 	// 0 for no limit
-	size_t user_limit = 0;
+	size_t user_limit;
 	std::vector<size_t> operator_user_id;
-	std::string topic = "UNDEFINED";
+	std::string topic;
 };
 
 class Channel
@@ -22,6 +23,7 @@ private:
 	std::vector<std::string> history;
 
 	channel_mode mode;
+	std::string name;
 
 public:
 };
