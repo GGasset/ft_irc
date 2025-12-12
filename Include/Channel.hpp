67d960c0612428fcc0489683930ee1355d64e149
Luis Felipe Ayala Sarabia <layala-s@c1r8s2.42madrid.com>
2025-12-12 15:04:42 +0100

    constructor added

--- Include/Channel.hpp
+++ Include/Channel.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include "User.hpp"
 #include "string"
 
 struct channel_mode
@@ -18,20 +19,21 @@ class Channel
 private:
 	size_t id = -1;
 	std::vector<size_t> member_user_ids;
-
 	// I don't know if this is correct (would be used for users who just joined the channel and the msg history would be sent)
 	// Just the msgs meant to be read by other people
 	std::vector<std::string> history;
-
+	std::vector<User *> users;
 	channel_mode mode;
 	std::string name;
 	std::string	topic = "";
 
 public:
-	ssize_t get_id();
+	Channel(void);
+	~Channel(void);
+	ssize_t get_id(void);
 	void set_id(ssize_t id);
-	std::string get_topic();
+	std::string get_topic(void);
 	void set_topic(std::string topic);
-	std::vector<size_t>	get_members();
-	std::string get_name();
+	std::vector<size_t>	get_members(void);
+	std::string get_name(void);
 };
--- srcs/Messaging/Channels/Channel.cpp
+++ srcs/Messaging/Channels/Channel.cpp
@@ -1,5 +1,7 @@
 #include "Channel.hpp"
 
+Channel::Channel(void){}
+Channel::~Channel(void){}
 ssize_t Channel::get_id() {return id;}
 void Channel::set_id(ssize_t id) {id = id;}
 std::vector<size_t>	Channel::get_members() {return member_user_ids;}
