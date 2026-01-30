d11de90441bb12651b399f49fb6156c53b9f8b65
GGasset <gassetgerman@gmail.com>
2026-01-30 08:40:39 +0100

    added extra error checking at main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 
+#include <cctype>
 #include <cstdlib>
 #include <exception>
 
@@ -12,15 +13,19 @@
 int main(int argc, char **argv)
 {
 	if (argc != 3)
-		{std::cerr << "Invalid parameters. " << USAGE_STR << std::endl; return 0;}
+		{std::cerr << "Invalid parameters count. " << USAGE_STR << std::endl; return 0;}
 
 	bool port_is_digits = !argv[1][0];
 	for (size_t i = 0; argv[1][i]; i++)
 		port_is_digits = port_is_digits && argv[1][i] >= '0' && argv[1][i] <= '9';
 	if (port_is_digits)
-		{std::cerr << "Invalid port paramter, only digits expected" << std::endl; return 0;}
-
+		{std::cerr << "Invalid PORT paramter, only digits expected >:(" << std::endl; return 0;}
 	int port = std::atoi(argv[1]);
+
+	bool pass_is_alnum = !argv[2][0];
+	for (size_t i = 0; argv[2][i]; i++) pass_is_alnum = pass_is_alnum && std::isalnum(argv[2][i]);
+	if (!pass_is_alnum)
+		{std::cerr << "Invalid PASSW paramter, only alpha numeric digits expected >:(" << std::endl; return 0;}
 
 	Server server(argv[2]);
 	if (server.loop(port))
